02d178146f4970090a0557d50491ff3cc8925152
jas112 <jas.sandmaster@gmail.com>
2024-08-07 13:02:09 -0700

    src assets: images prt 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>src assets: audio + documents</h1>
+      <h1>src assets: images prt 2</h1>
     </>
   )
 }
