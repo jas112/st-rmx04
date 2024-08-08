3269af0550d6235a5c27c3e1f157564862960dd4
jas112 <jas.sandmaster@gmail.com>
2024-08-08 05:13:42 -0700

    modified v-config base 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jas112.github.io/st-rmx04/",
+  base: "/st-rmx04/",
   plugins: [react()],
 })
